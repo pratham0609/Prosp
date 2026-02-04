208369a3f869864e77092a4951bac9cc10514cfa
pratham0609 <pndandale@hotmail.com>
2026-02-04 17:18:48 +0530

    fix: railway redis connection and server build output

--- api/src/queue/redis.ts
+++ api/src/queue/redis.ts
@@ -1,7 +1,14 @@
-import IORedis from 'ioredis'
+// import IORedis from 'ioredis'
 
-export const redisConnection = new IORedis({
-  host: '127.0.0.1',
-  port: 6379,
-  maxRetriesPerRequest: null
-})
+// export const redisConnection = new IORedis({
+//   // host: '127.0.0.1',
+//   // port: 6379,
+//   url: process.env.REDIS_URL,
+//   maxRetriesPerRequest: null
+// })
+/////////////ABOVE FOR LOCAL TESTING, BELOW FOR DEPLOYMENT /////////////  
+import { ConnectionOptions } from 'bullmq'
+
+export const redisConnection: ConnectionOptions = {
+  url: process.env.REDIS_URL
+}
